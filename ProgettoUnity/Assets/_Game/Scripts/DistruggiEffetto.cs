f0be6e9e68183c6ad2df226d5250913acca069ef
Pino da Mac <pino@dftgames.com>
2014-08-19 21:38:27 +0100

    Quarta lezione - Eventi

---
+++ ProgettoUnity/Assets/_Game/Scripts/DistruggiEffetto.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class DistruggiEffetto : MonoBehaviour {
+
+	private ParticleSystem effetto;
+
+	// Use this for initialization
+	void Start () {
+		effetto = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!effetto.isPlaying)
+			Destroy (gameObject);
+	}
+}
---
+++ ProgettoUnity/Assets/_Game/Scripts/Pedate.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pedate : MonoBehaviour {
+
+	public GameObject effetto;
+	public Transform piedeDx;
+	public Transform piedeSx;
+
+	void SxPedata()
+	{
+		Instantiate (effetto, piedeSx.position, Quaternion.identity);
+	}
+	
+	void DxPedata()
+	{
+		Instantiate (effetto, piedeDx.position, Quaternion.identity);
+	}
+	
+}
