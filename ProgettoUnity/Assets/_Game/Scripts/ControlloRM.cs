8484679a900dde53d011a70fdc678f64c08edd36
Giuseppe De Francesco <pino@dftgames.com>
2014-09-10 22:35:53 +0100

    Alla puntata #6

---
+++ ProgettoUnity/Assets/_Game/Scripts/ControlloRM.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Animator))]
+public class ControlloRM : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+            z /= 2f;
+        anim.SetFloat("direzione", x);
+        anim.SetFloat("velocita", z);
+    }
+}
