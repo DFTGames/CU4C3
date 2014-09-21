6768e148f71e69d9a090dcb34769b592ae06d773
Giuseppe De Francesco <pino@dftgames.com>
2014-09-21 13:17:04 +0100

    Alla lezione 4.2

---
+++ ProgettoUnity/Assets/_Game/Scripts/Insegui.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Insegui : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private Transform giocatoreT;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        giocatoreT = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(giocatoreT.position);
+    }
+}
