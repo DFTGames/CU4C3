0ff5bf3cd5539b797a401988a14725a3164ed6bf
Giuseppe De Francesco <pino@dftgames.com>
2014-09-21 03:13:41 +0100

    Alla lezione 4.1

---
+++ ProgettoUnity/Assets/_Game/Scripts/PuntaEClicca.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PuntaEClicca : MonoBehaviour {
+
+    protected NavMeshAgent agent;
+    protected RaycastHit hit;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void AttivitaMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            agent.SetDestination(hit.point);
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            AttivitaMouse();
+        }
+    }
+}
